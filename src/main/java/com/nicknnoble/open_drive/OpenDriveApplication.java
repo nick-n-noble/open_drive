22ceb2946a67897cfc056a5c7266fbda50dcd4c9
Nick Noble <nicholas100200@gmail.com>
2024-06-19 03:29:52 -0500

    removed imports

--- src/main/java/com/nicknnoble/open_drive/OpenDriveApplication.java
+++ src/main/java/com/nicknnoble/open_drive/OpenDriveApplication.java
@@ -3,8 +3,6 @@ package com.nicknnoble.open_drive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nicknnoble.open_drive.security.SecurityConstants;
-
 @SpringBootApplication
 public class OpenDriveApplication {
 
