9d2e8a1cd3851494bcbd86b95f4dd733e000dbcf
Nick Noble <nicholas100200@gmail.com>
2024-06-28 03:04:33 -0500

    react-router-dom

--- open_drive_frontend/src/App.jsx
+++ open_drive_frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Register from './components/Register'
 import Login from './components/Login'
+import Users from './components/Users'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <header className='App-header'>
-        {/* <Register /> */}
-        <Login />
-      </header>
-    </div>
+    <Routes>
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/test' element={<Users />} />
+    </Routes>
   )
 }
 
